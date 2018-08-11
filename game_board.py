410ceb8981379c5fb0dc5f3bfbc3313d002fd49c
BaSiczek <barbara.siczek@tooploox.com>
2018-08-11 12:23:45 +0200

    Move batch creation outside the on_draw() function
    Move batch preparation from @window.event -> on_draw() function. This 
    way board and cells will be created one time in the beggining(not in 
    every frame)
    
    Add also docstrings and make styling changes from PEP-8 to Google.

--- game_board.py
+++ game_board.py
@@ -2,11 +2,19 @@ import pyglet
 
 
 class Cell:
-    def __init__(self, position_x, position_y, cell_size, state):
-        self.position_x = position_x    # position is bottom left corner
+
+    def __init__(self, position_x, position_y, cell_size):
+        """
+        Args:
+            position_x (int): position X (horisontal) in pixels
+                of a bottom left corner.
+            position_y (int): position Y (vertical) in pixels
+                of a bottom left corner.
+            cell_size (int): size in pixels of a square cell
+        """
+        self.position_x = position_x
         self.position_y = position_y
         self.cell_size = cell_size
-        self.state = state
 
     def add_cell_to_batch(self, batch):
         left = self.position_x
@@ -14,31 +22,47 @@ class Cell:
         bottom = self.position_y
         top = self.position_y + self.cell_size
 
-        vertex_list = batch.add(5, pyglet.gl.GL_LINE_STRIP, None,
-                                ('v2i', (left, bottom,
-                                         right, bottom,
-                                         right, top,
-                                         left, top,
-                                         left, bottom)),
-                                ('c3B', (0, 0, 255,
-                                         0, 0, 255,
-                                         0, 0, 255,
-                                         0, 0, 255,
-                                         0, 0, 255)))
+        vertex_list = batch.add(
+            5,
+            pyglet.gl.GL_LINE_STRIP,
+            None,
+            ('v2i',
+            (left, bottom, right, bottom, right, top, left, top, left, bottom)),
+            ('c3B',(0, 0, 255, 0, 0, 255, 0, 0, 255, 0, 0, 255, 0, 0, 255))
+        )
 
-
-def draw_background(win_width, win_height,
-                    board_width, board_height, columns_num, rows_num):
-    board_vertices = calculate_board_vertices(win_width, win_height,
-                                              board_width, board_height)
-    cell_size = calculate_cell_size(board_width, board_height,
-                                    columns_num, rows_num)
+def prepare_batch(
+    win_width, win_height, board_width, board_height, columns_num, rows_num):
+    """
+    Args:
+        win_width, win_height, board_width, board_height (int):
+            width and height of an aplication Window and the Board (game area)
+        columns_num, rows_num (int): number of columns and rows on the Board
+    Returns:
+        batch: (pyglet.graphics.Batch()) for drawing white board with blue grid
+            (square cells)
+    """
     batch = pyglet.graphics.Batch()
-    draw_board(board_vertices, batch)
-    draw_cells(board_vertices[0][0], board_vertices[0][1],
-               cell_size, columns_num, rows_num, batch)
-    batch.draw()
+    board_vertices = calculate_board_vertices(
+        win_width, win_height, board_width, board_height
+     )
+    cell_size = calculate_cell_size(
+        board_width, board_height, columns_num, rows_num
+    )
+    add_board_to_batch(board_vertices, batch)
+# create_cells will also add them into the batch
+    create_cells(
+        board_vertices[0][0],
+        board_vertices[0][1],
+        cell_size,
+        columns_num,
+        rows_num,
+        batch
+    )
+    return batch
 
+def draw_background(batch):
+    batch.draw()
 
 def calculate_cell_size(board_width, board_height, columns_num, rows_num):
     if (board_width // columns_num == board_height // rows_num and
@@ -51,31 +75,37 @@ def calculate_cell_size(board_width, board_height, columns_num, rows_num):
                          "columns/rows. Cells must be square.")
 
 
-def draw_board(board_vertices, batch):
-    batch.add(4, pyglet.gl.GL_QUADS, None,
-              ('v2i', (board_vertices[0][0], board_vertices[0][1],
-                       board_vertices[1][0], board_vertices[1][1],
-                       board_vertices[2][0], board_vertices[2][1],
-                       board_vertices[3][0], board_vertices[3][1]))
-              )
+def add_board_to_batch(board_vertices, batch):
+    batch.add(
+        4,
+        pyglet.gl.GL_QUADS,
+        None,
+        ('v2i', (
+            board_vertices[0][0],
+            board_vertices[0][1],
+            board_vertices[1][0],
+            board_vertices[1][1],
+            board_vertices[2][0],
+            board_vertices[2][1],
+            board_vertices[3][0],
+            board_vertices[3][1]))
+        )
 
 
-def draw_cells(board_left, board_bottom, cell_size,
-               columns_num, rows_num, batch):
+def create_cells(
+    board_left, board_bottom, cell_size, columns_num, rows_num, batch):
     position_x = board_left
     position_y = board_bottom
     for column in range(rows_num):
         for row in range(columns_num):
-            cell = Cell(position_x, position_y, cell_size, 0)
+            cell = Cell(position_x, position_y, cell_size)
             cell.add_cell_to_batch(batch)
             position_x += cell_size
         position_x = board_left
-
-# the drawing cursor must return to the starting position of the new row:
+#the drawing cursor must return to the starting position of the new row:
         batch.add(1, pyglet.gl.GL_LINE_STRIP, None,
                   ('v2i', (position_x, position_y)),
                   ('c3B', (0, 0, 255)))
-
         position_y += cell_size
 
 
@@ -94,6 +124,3 @@ def calculate_board_vertices(win_width, win_height, board_width, board_height):
         return board_vertices
     else:
         raise ValueError("The Window size must be bigger than the Board size")
-
-if __name__ == '__main__':
-    pyglet.app.run()
--- main.py
+++ main.py
@@ -7,11 +7,12 @@ window = pyglet.window.Window(1280, 720,
                               resizable=False
                               )
 
-
+background_batch = game_board.prepare_batch(
+    window.width, window.height, 800, 450, 16, 9)
 @window.event
 def on_draw():
     window.clear()
-    game_board.draw_background(window.width, window.height, 800, 450, 16, 9)
+    game_board.draw_background(background_batch)
 
 if __name__ == '__main__':
     pyglet.app.run()
